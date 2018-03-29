6e8dc9782620aa6f7fcb9f9b63113a2995693569
Chris Pressey <cpressey@catseye.tc>
2018-03-29 12:11:38 +0100

    Fork the Atari 2600 Ophis example for easier comparing to 60p.

--- eg/atari2600/build.sh
+++ eg/atari2600/build.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
-sixtypical --prelude=atari2600 atari-2600-example.60p > atari-2600-example-60p.bin
+sixtypical --prelude=atari2600 smiley.60p > smiley-60p.bin
 if [ "x$COMPARE" != "x" ]; then
-  ophis atari-2600-example.oph -o atari-2600-example.bin
-  dcc6502 -o 0xf000 -m 200 atari-2600-example.bin > atari-2600-example.bin.disasm.txt
-  dcc6502 -o 0xf000 -m 200 atari-2600-example-60p.bin > atari-2600-example-60p.bin.disasm.txt
-  paste atari-2600-example.bin.disasm.txt atari-2600-example-60p.bin.disasm.txt | pr -t -e24
-  #diff -ru atari-2600-example.bin.disasm.txt atari-2600-example-60p.bin.disasm.txt
+  ophis smiley.oph -o smiley.bin
+  dcc6502 -o 0xf000 -m 200 smiley.bin > smiley.bin.disasm.txt
+  dcc6502 -o 0xf000 -m 200 smiley-60p.bin > smiley-60p.bin.disasm.txt
+  paste smiley.bin.disasm.txt smiley-60p.bin.disasm.txt | pr -t -e24
 fi
